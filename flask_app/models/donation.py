02b110dc1022df4ff6a27327e8cad8e8622fc3ce
Anant Dhokia <anantdhokia@gmail.com>
2023-08-08 19:51:02 -0700

    added donations and student_sponsorships backend functionality

---
+++ flask_app/models/donation.py
@@ -0,0 +1,25 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+#from flask_app.models import thought
+from flask import flash
+import re
+
+class Donation:
+    def __init__(self, data):
+        self.id = data['id']
+        self.amount = data['amount']
+        self.data = data['date']
+        self.method = data['method']
+        self.event = data['event']
+        self.notes = data['notes']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.member_id = data['member_id']
+        self.member_creator = None
+
+
+    @classmethod
+    def create_donation(cls, data):
+        query = "INSERT INTO donations (amount, date, method, event, notes, created_at, updated_at, member_id) VALUES (%(amount)s, %(date)s, %(method)s, %(event)s, %(notes)s, %(created_at)s, %(updated_at)s, %(member_id)s);"
+        result = connectToMySQL('sandipani').query_db(query, data)
+
+        return result
